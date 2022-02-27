33f513ea8df36bee5c5e332d9ad481c4c3e38c75
sunilkumarmandati <sunilkumar.mandadi@gmail.com>
2022-02-27 13:12:23 -0800

    initial commit

---
+++ LogElasticsearchKibanaAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogElasticsearchKibanaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation($"Weather Controller executed at {DateTime.Now.Date}");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("{exception}")]
+        public IEnumerable<WeatherForecast> Get(string exception)
+        {
+            try
+            {
+                throw new Exception("Test exception");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Weather Controller exception: {ex.Message} at: {DateTime.Now.Date}");
+                throw;
+            }
+        }
+    }
+}
